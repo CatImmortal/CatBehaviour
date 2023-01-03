0a3771be06e248592ec452d4f53d91c0dca54946
CatImmortal <1304085906@qq.com>
2023-01-03 09:39:55 +0800

    Update BaseBBParamDrawer.cs

--- Assets/Scripts/CatBehaviour/Editor/BBParamDrawer/BaseBBParamDrawer.cs
+++ Assets/Scripts/CatBehaviour/Editor/BBParamDrawer/BaseBBParamDrawer.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Reflection;
 using CatBehaviour.Runtime;
-using Unity.VisualScripting.YamlDotNet.Core.Tokens;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.UIElements;
